8b0172aab1464de85ac39f57acd8aead8fd7df00
joebbrian <joebbrian@thezig.io>
2024-06-26 23:05:19 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'new-component', component: MyNewComponentComponent },
+  { path: 'login', component: LoginComponent },
+];
+
+
